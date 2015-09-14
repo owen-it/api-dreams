6a3e8a3b1789735a85291fce6de4f9d9dda39b14
Antério vieira da silva <anteriovieira@gmail.com>
2015-09-14 11:20:23 -0300

    Audit implemented in users

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -9,8 +9,9 @@ use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use OwenIt\Auditing\Auditing;
 
-class User extends Model implements AuthenticatableContract,
+class User extends Auditing implements AuthenticatableContract,
                                     AuthorizableContract,
                                     CanResetPasswordContract
 {
